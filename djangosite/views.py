05d490d002c5df22e285d2c18a311c73f4e8afd7
henryxian <henryxian@qq.com>
2015-11-28 11:31:21 +0800

    fix import

--- djangosite/views.py
+++ djangosite/views.py
@@ -2,6 +2,7 @@ from django.template.loader import get_template
 from django.template import Context
 from django.template import Template
 from django.http import HttpResponse
+from django.shortcuts import Http404
 
 from django.shortcuts import render_to_response
 import datetime
