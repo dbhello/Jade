cc5067810aa1853dcf278a86d4cb99523726fc05
henryxian <henryxian@qq.com>
2015-11-28 13:12:03 +0800

    refactor

--- djangosite/settings.py
+++ djangosite/settings.py
@@ -38,7 +38,6 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'djangosite.books',
     'djangosite.supermarket',
 )
 
@@ -89,4 +88,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.6/howto/static-files/
 
 STATIC_URL = "/static/"
-STATIC_ROOT = os.path.join(currentDir, 'static/').replace('\\','/')
+STATIC_ROOT = os.path.join(currentDir, 'static/').replace('\\', '/')
